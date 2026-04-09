81ef2e035453738a6669e93f2d2977e2b110b9c6
Alfred-Johnson <99013661+Alfred-Johnson@users.noreply.github.com>
2026-04-09 16:31:35 +0530

    Update duck.sh

--- duckdns/duck.sh
+++ duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=alfred-devops&token=YOUR_TOKEN&ip=" | curl -k -o duck.log -K -
+echo url="https://www.duckdns.org/update?domains=alfred-devops&token=5783924a-59e1-4dcd-8783-9295979914db&ip=" | curl -k -o duck.log -K -
